9c0ae16f369588da62c60273042e67319c5bd7d0
NormalFall <normfalldev@pm.me>
2025-12-14 15:05:28 -0500

    launcher 2.11.10 -> 2.13.1

--- slippi/launcher.nix
+++ slippi/launcher.nix
@@ -9,11 +9,11 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "slippi-launcher";
-  version = "2.11.10";
+  version = "2.13.1";
 
   src = fetchurl {
     url = "https://github.com/project-slippi/slippi-launcher/releases/download/v${version}/Slippi-Launcher-${version}-x86_64.AppImage";
-    hash = "sha256-OrWd0jVqe6CzNbVRNlm2alt2NZ8uBYeHiASaB74ouW4=";
+    hash = "sha256-bKMwRqFqZ5AnctdvtINrsO1I5uCBni+WFmNsV4ykBNQ=";
   };
   dontUnpack = true;
 
